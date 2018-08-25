c41df5bedb27b5ecdfd85a39220f821615a4ea2b
姜灏 <150haomengting@163.com>
2018-08-25 09:10:53 +0800

    init project

---
+++ demo/Demo.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="p-relative">
+        <p class="container x92 y40 ym10 b-border t-pointer x-between" @click="showProduct=!showProduct">
+            <span>{{selectProduct||'选择产品'}}</span>
+            <span>
+                <i class="ym5 fa fa-sort" aria-hidden="true"></i>
+            </span>
+        </p>
+        <v-block :show="showProduct" :size="'x92 y240'">
+            <div slot="block">
+                <ul>
+                    <li class="y30 t-pointer" :class="{'t-current':item === selectProduct}" v-for="(item,index) in options" :key="index" @click="checkProduct(item)">{{item}}</li>
+                </ul>
+            </div>
+        </v-block>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            showProduct: 0,
+            selectProduct: undefined,
+
+            options: [
+                'aaa',
+                'bbb',
+                'ccc',
+                'eee',
+                'fff',
+                'xxx',
+                'yyy',
+                'zzz'
+            ]
+        }
+    },
+    methods: {
+        checkProduct(item) {
+            this.selectProduct = item
+            this.showProduct = 0
+        }
+    }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
+
---
+++ src/Block.vue
@@ -0,0 +1,14 @@
+<template>
+    <article class="bgf b-border p-absolute o-auto" :class="size" v-show="show">
+        <slot name="block"></slot>
+    </article>
+</template>
+<script>
+export default {
+    props: ["show", "size"],
+    data() {
+        return {}
+    }
+}
+</script>
+<style scoped></style>
